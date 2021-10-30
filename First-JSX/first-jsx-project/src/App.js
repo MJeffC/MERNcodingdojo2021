8b186e19891fed18914639215747ac18e7668444
MJeffC <mjeffcastillo@gmail.com>
2021-10-30 01:13:07 -0400

    First React Assignment

---
+++ First-JSX/first-jsx-project/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import HelloDojo from './components/Hello-Dojo';
+
+function App() {
+  return (
+    <div className="App">
+      <HelloDojo />
+    </div>
+  );
+}
+
+export default App;
---
+++ First-JSX/first-jsx-project/src/components/Hello-Dojo.js
@@ -0,0 +1,14 @@
+const HelloDojo = () => {
+    return (
+        <>
+            <h1 style={{textAlign: "left", margin: "10px"}}>Hello Dojo!</h1>
+            <h2 style={{textAlign: "left", margin: "10px"}}>Things I need to do:</h2>
+            <li style={{textAlign: "left", margin: "10px"}}>Learn React</li>
+            <li style={{textAlign: "left", margin: "10px"}}>Climb Mt. Everest</li>
+            <li style={{textAlign: "left", margin: "10px"}}>Run a marathon</li>
+            <li style={{textAlign: "left", margin: "10px"}}>Feed the dogs</li>
+        </>
+    );
+};
+
+export default HelloDojo;
