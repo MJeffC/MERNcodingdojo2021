aa2e522b734f67806fc4f8070e840a4729b3cef9
MJeffC <mjeffcastillo@gmail.com>
2021-11-03 21:21:03 -0400

    Prop It Up Assignment

---
+++ Prop-It-Up/prop-it-up/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/PropItUp';
+
+function App() {
+  return (
+    <div className="App">
+        <Header firstName = {"Jane"} lastName = {"Doe"} age = {"45"} hairColor = {"Black"} />
+        <Header firstName = {"John"} lastName = {"Smith"} age = {"88"} hairColor = {"Brown"} />
+        <Header firstName = {"Millard"} lastName = {"Fillmore"} age = {"50"} hairColor = {"Brown"} />
+        <Header firstName = {"Maria"} lastName = {"Smith"} age = {"62"} hairColor = {"Brown"} />
+    </div>
+  );
+}
+
+export default App;
