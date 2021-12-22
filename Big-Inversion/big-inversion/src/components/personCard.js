41ae3b618a5d63fd861ce29e40553b2614b2b16d
MJeffC <mjeffcastillo@gmail.com>
2021-12-22 00:55:36 -0500

    Big Inversion Assignment

---
+++ Big-Inversion/big-inversion/src/components/personCard.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class PersonCard extends Component {
+    render() {
+        const { lastName, firstName, age, hairColor } = this.props;
+        return (
+            <div>
+                <h1>{lastName}, {firstName}</h1>
+                <h4>Age: {age}</h4>
+                <h4>Hair Color: {hairColor}</h4>
+            </div>
+        );
+    }
+}
+
+export default PersonCard;
